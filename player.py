c208cfdbd55aa383e37ea61f73a0f810d8fcb7c1
ARKtec8bit <alan.r.kissick@gmail.com>
2021-09-18 16:07:45 +1200

    poo ``

--- player.py
+++ player.py
@@ -7,8 +7,11 @@ class Player(pygame.sprite.Sprite):
         self.image = pygame.Surface((32, 64))
         self.image.fill("green")
         self.rect = self.image.get_rect(topleft=pos)
+        
         self.direction = pygame.math.Vector2(0, 0)
         self.speed = 8
+        self.gravity = 0.8
+        self.jump_speed = -16
 
     def get_input(self):
         keys = pygame.key.get_pressed()
@@ -19,6 +22,8 @@ class Player(pygame.sprite.Sprite):
             self.direction.x = -1
         else:
             self.direction.x = 0
+
+    def 
 
     def update(self):
         self.get_input()
