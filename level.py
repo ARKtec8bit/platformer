d8767582e414d9b7568a72ad6199f4bbfdba4fcb
ARKtec8bit <alan.r.kissick@gmail.com>
2021-09-18 15:12:37 +1200

    player movement

--- level.py
+++ level.py
@@ -1,6 +1,8 @@
 import pygame
+from pygame import sprite
 from tiles import Tile
 from settings import tile_size
+from player import Player
 
 
 class Level:
@@ -14,6 +16,8 @@ class Level:
     def setup_level(self, layout):
 
         self.tiles = pygame.sprite.Group()
+        self.player = pygame.sprite.GroupSingle()
+
         for row_index, row in enumerate(layout):
             for col_index, cell in enumerate(row):
                 x = col_index * tile_size
@@ -24,8 +28,30 @@ class Level:
                 if cell == ".":
                     tile = Tile((x, y), tile_size, "cyan")
                     self.tiles.add(tile)
+                if cell == "P":
+                    player_sprite = Player((x, y))
+                    self.player.add(player_sprite)
+
+    def scroll_x(self):
+        player = self.player.sprite
+        player_x = player.rect.centerx
+        direction_x = player.direction.x
+
+        if player_x < 200 and direction_x < 0:
+            self.world_shift = 8
+            player.speed = 0
+        elif player_x > 1000 and direction_x > 0:
+            self.world_shift = -8
+            player.speed = 0
+        else:
+            self.world_shift = 0
+            player.speed = 8
 
     def run(self):
 
-        self.tiles.update(-1)
+        self.tiles.update(self.world_shift)
         self.tiles.draw(self.display_surface)
+
+        self.player.update()
+        self.player.draw(self.display_surface)
+        self.scroll_x()
