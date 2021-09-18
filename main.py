2b5b68557f35e01fa34e51fc0c0c852ad52d6a21
Alan <alan.r.kissick@gmail.com>
2021-09-18 13:48:22 +1200

    img

--- main.py
+++ main.py
@@ -4,20 +4,20 @@ from settings import *
 from tiles import Tile
 from level import Level
 
+if __name__ == "__main__":
+    pygame.init()
+    screen = pygame.display.set_mode((screen_width, screen_height))
+    clock = pygame.time.Clock()
+    level = Level(level_map, screen)
 
-pygame.init()
-screen = pygame.display.set_mode((screen_width, screen_height))
-clock = pygame.time.Clock()
-level = Level(level_map, screen)
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
 
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
+        screen.fill("black")
+        level.run()
 
-    screen.fill("black")
-    level.run()
-
-    pygame.display.update()
-    clock.tick(60)
+        pygame.display.update()
+        clock.tick(60)
