49626c5e1bcba0035eb15be1655a3e98ac08ad05
ARKtec8bit <alan.r.kissick@gmail.com>
2021-09-23 19:38:42 +1200

    f

--- level.py
+++ level.py
@@ -25,9 +25,9 @@ class Level:
                 if cell == "X":
                     tile = Tile((x, y), tile_size, "red")
                     self.tiles.add(tile)
-                if cell == ".":
-                    tile = Tile((x, y), tile_size, "cyan")
-                    self.tiles.add(tile)
+                # if cell == ".":
+                #     tile = Tile((x, y), tile_size, "cyan")
+                #     self.tiles.add(tile)
                 if cell == "P":
                     player_sprite = Player((x, y))
                     self.player.add(player_sprite)
@@ -47,11 +47,39 @@ class Level:
             self.world_shift = 0
             player.speed = 8
 
+    def horizontal_movement_collision(self):
+
+        player = self.player.sprite
+        player.rect.x += player.direction.x * player.speed
+
+        for sprite in self.tiles.sprites():
+            if sprite.rect.colliderect(player.rect):
+                if player.direction.x < 0:
+                    player.rect.left = sprite.rect.right
+                elif player.direction.x > 0:
+                    player.rect.right = sprite.rect.left
+
+    def vert_movement_collision(self):
+
+        player = self.player.sprite
+        player.apply_gravity()
+
+        for sprite in self.tiles.sprites():
+            if sprite.rect.colliderect(player.rect):
+                if player.direction.y > 0:
+                    player.rect.bottom = sprite.rect.top
+                    player.direction.y = 0
+                elif player.direction.y < 0:
+                    player.rect.top = sprite.rect.bottom
+                    player.direction.y = 0
+
     def run(self):
 
         self.tiles.update(self.world_shift)
         self.tiles.draw(self.display_surface)
+        self.scroll_x()
 
         self.player.update()
+        self.horizontal_movement_collision()
+        self.vert_movement_collision()
         self.player.draw(self.display_surface)
-        self.scroll_x()
