750140de5b998b6d6f5981827673282c9a5928c9
ARKtec8bit <alan.r.kissick@gmail.com>
2021-09-25 10:08:39 +1200

    animation, colissions and some particles

--- level.py
+++ level.py
@@ -1,5 +1,6 @@
 import pygame
 from pygame import sprite
+from pygame.constants import TIMER_RESOLUTION
 from tiles import Tile
 from settings import tile_size, screen_width
 from player import Player
@@ -12,6 +13,7 @@ class Level:
         self.display_surface = surface
         self.setup_level(level_data)
         self.world_shift = 0
+        self.current_x = 0
 
     def setup_level(self, layout):
 
@@ -29,7 +31,7 @@ class Level:
                 #     tile = Tile((x, y), tile_size, "cyan")
                 #     self.tiles.add(tile)
                 if cell == "P":
-                    player_sprite = Player((x, y))
+                    player_sprite = Player((x, y), self.display_surface)
                     self.player.add(player_sprite)
 
     def scroll_x(self):
@@ -56,8 +58,20 @@ class Level:
             if sprite.rect.colliderect(player.rect):
                 if player.direction.x < 0:
                     player.rect.left = sprite.rect.right
+                    player.on_left = True
+                    self.current_x = player.rect.left
                 elif player.direction.x > 0:
                     player.rect.right = sprite.rect.left
+                    player.on_right = True
+                    self.current_x = player.rect.right
+        if player.on_left and (
+            player.rect.left < self.current_x or player.direction.x >= 0
+        ):
+            player.on_left = False
+        if player.on_right and (
+            player.rect.right > self.current_x or player.direction.x >= 0
+        ):
+            player.on_right = False
 
     def vert_movement_collision(self):
 
@@ -69,9 +83,16 @@ class Level:
                 if player.direction.y > 0:
                     player.rect.bottom = sprite.rect.top
                     player.direction.y = 0
+                    player.on_ground = True
                 elif player.direction.y < 0:
                     player.rect.top = sprite.rect.bottom
                     player.direction.y = 0
+                    player.on_ceiling = True
+
+        if player.on_ground and player.direction.y < 0 or player.direction.y > 1:
+            player.on_ground = False
+        if player.on_ground and player.direction.y > 0:
+            player.on_ceiling = False
 
     def run(self):
 
